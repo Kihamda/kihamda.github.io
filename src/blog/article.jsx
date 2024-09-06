e1f9d4f6faff65c05f0d9bb60c5d3cda4c634d6d
Dai <kuawse580@gmail.com>
2024-09-06 12:02:57 +0900

    Auto commit

--- src/blog/article.jsx
+++ src/blog/article.jsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import Markdown from "react-markdown";
 
 const Article = () => {
   const locate = useLocation();
   const id = locate.pathname.slice(6);
+  const [contents, setContents] = useState();
+
+  useEffect(() => {
+    (async () => {
+      console.log("useEffect in Article");
+      // fetch data from API or local storage by id
+      const [content, extra] = await getArticle(id);
+      setContents(content);
+    })();
+  }, [locate]);
+
   console.log(id);
+  return <Markdown>{contents}</Markdown>;
 };
 const BlogList = () => {
   return <p>BLOG LIST</p>;
 };
-export { Article, BlogList };
+
+const getArticle = async (id) => {
+  const article = fetch(
+    `https://kihamda.github.io/blogcms/articles/${id}/text.md`
+  ).then((data) => {
+    return data.text();
+  });
+  const extra = fetch(
+    `https://kihamda.github.io/blogcms/articles/${id}/extra.json`
+  ).then((data) => {
+    return data.json();
+  });
+
+  const txt = await article;
+  const ext = await extra;
+  return [txt, ext];
+};
+
+export { Article, BlogList, getArticle };
--- src/blog/blog.jsx
+++ src/blog/blog.jsx
@@ -6,39 +6,33 @@ const Blog = () => {
     <>
       <div className="container mt-3">
         <div className="row">
-          <div className="col-md-3 d-none d-lg-block">
-            <h2>カテ��リー</h2>
-            <ul>
-              <li>
-                <a href="#">カテ��リー1</a>
-              </li>
-              <li>
-                <a href="#">カテ��リー2</a>
-              </li>
-              <li>
-                <a href="#">カテ��リー3</a>
-              </li>
-            </ul>
+          <div className="col-md-8 col-12">
+            <div className="card">
+              <div className="card-body">
+                <Routes>
+                  <Route path="/" element={<BlogList />} />
+                  <Route path="/:id" element={<Article />} />
+                </Routes>
+              </div>
+            </div>
           </div>
-          <div className="col-md-6 col-12">
-            <Routes>
-              <Route path="/" element={<BlogList />} />
-              <Route path="/:id" element={<Article />} />
-            </Routes>
-          </div>
-          <div className="col-md-3 d-none d-lg-block">
-            <h2>カテ��リー</h2>
-            <ul>
-              <li>
-                <a href="#">カテ��リー1</a>
-              </li>
-              <li>
-                <a href="#">カテ��リー2</a>
-              </li>
-              <li>
-                <a href="#">カテ��リー3</a>
-              </li>
-            </ul>
+          <div className="col-md-4 d-none d-lg-block">
+            <div className="card">
+              <div className="card-header">記事一覧</div>
+              <div className="card-body">
+                <ul>
+                  <li>
+                    <a href="#">記事1</a>
+                  </li>
+                  <li>
+                    <a href="#">記事2</a>
+                  </li>
+                  <li>
+                    <a href="#">記事3</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
